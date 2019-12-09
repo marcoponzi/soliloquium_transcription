b6eed529151c3ea67e1184d9e0822bd9740cc936
Marco Ponzi <marco.ponzi@gmail.com>
2019-12-09 08:56:02 +0100

    count multiple occurrences on one line

--- code/do_freq.sh
+++ code/do_freq.sh
@@ -5,5 +5,5 @@ cat $1 | sed -e 's/ /  /g' | sed -e 's/^/ /' | sed -e 's/$/ /' > /tmp/spaces
 for w in `cat /tmp/words`
 do
 echo -n $w' '
-grep -F ' '$w' ' /tmp/spaces| wc -l
+grep -oF ' '$w' ' /tmp/spaces| wc -l
 done | sort -rnk 2 > 'freq_'$1
